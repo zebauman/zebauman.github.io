6caac6b597c696e4fc74b04832b1d13965a0e75d
zach <zach.bauman12@gmail.com>
2025-12-13 15:44:04 -0600

    potential page fix?

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PortfolioLayout from "./components/layout/PortfolioLayout.tsx";
 
 export default function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://zbauman.github.io", // <- replace with your GitHub repo name (trailing slash)
+  base: "/Portfolio/", // <- replace with your GitHub repo name (trailing slash)
   plugins: [react()]
 });
