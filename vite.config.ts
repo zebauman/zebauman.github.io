858113265526842919e8ec79c43858ee7f778096
zach <zach.bauman12@gmail.com>
2025-12-13 16:03:25 -0600

    test publish site

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio/", // <- replace with your GitHub repo name (trailing slash)
   plugins: [react()]
 });
